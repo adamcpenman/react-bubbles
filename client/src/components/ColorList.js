a831bf15c208793327ebb72dd637553a721e6eac
Your Name <you@example.com>
2019-11-09 11:29:13 -0600

    refactored my axios util.Fetched color data in Bubblepage

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -18,13 +19,11 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+ 
   };
 
   return (
